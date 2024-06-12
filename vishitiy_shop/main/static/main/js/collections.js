5a62d1f2ccc8d6b983092d00938b2e7d4023d042
danilgubarev2008 <gubarevdanil83@gmail.com>
2024-06-12 17:44:01 +0300

    add big updates

---
+++ vishitiy_shop/main/static/main/js/collections.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Обработка кликов на карточки
+    const collectionCards = document.querySelectorAll('.collect');
+    collectionCards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            const collectionId = card.getAttribute('data-collection-id');
+            if (collectionId) {
+                const newUrl = `/products/?type=&collection=${collectionId}`;
+                window.location.href = newUrl;
+            } else {
+                console.error('Collection ID not found');
+            }
+        });
+    });
+});
