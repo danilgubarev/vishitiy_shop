4ab678e3a56e5e54be13eeb0975fd2b413ff59c8
Alexey <alexxey2707@gmail.com>
2024-06-15 10:08:12 +0200

    fixed merge issues

--- vishitiy_shop/products/static/products/js/detail.js
+++ vishitiy_shop/products/static/products/js/detail.js
@@ -1,47 +1,36 @@
 import { HandleCounter } from "/static/js/counter.js";
 import { showToast } from "/static/js/notifications.js";
-
+ 
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector('#add-to-cart-form');
-    if (form) {
-        form.addEventListener('submit', addToCart);
-    }
 
+    const form = document.querySelector('#add-to-cart-form');
+    form.addEventListener('submit', addToCart);
     const decrementButton = document.querySelector('.product-decrement-btn');
     const incrementButton = document.querySelector('.product-increment-btn');
+    new HandleCounter(decrementButton, incrementButton);
+})
 
-    if (decrementButton && incrementButton) {
-        new HandleCounter(decrementButton, incrementButton);
-    } else {
-        console.error('Decrement or Increment button not found');
-    }
-});
 
 function addToCart(e) {
   e.preventDefault();
-  const form = e.target;
-  const formdata = new FormData(form);
+  const form = e.target
+  const formdata = new FormData(form)
   const sizeSelected = form.querySelector('input[name="size"]:checked');
   const colorSelected = form.querySelector('input[name="color"]:checked');
   const url = window.location.origin + '/cart/add/';
-
+  
   if (!sizeSelected || !colorSelected) {
-    return showToast('Будь ласка, оберіть розмір та колір перед додаванням до кошика', 'warning');
+    return showToast('Будь ласка, оберіть розмір та кольор перед додаванням до кошика', 'warning');
   }
 
   const options = {
     method: 'POST',
     body: formdata,
   };
-
   fetch(url, options)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       if (data.errors) {
         for (const [key, value] of Object.entries(data.errors)) {
           showToast(value, 'danger');
@@ -52,6 +41,6 @@ function addToCart(e) {
     })
     .catch((error) => {
       console.error('Error:', error);
-      showToast('Виникла помилка при додаванні товару до кошика', 'danger');
+      showToast('Произошла ошибка при добавлении в корзину', 'danger');
     });
 }
