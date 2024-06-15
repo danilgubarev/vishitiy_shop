99e8df2cc54934815ab0a1c4f7e4cba797e955c0
Alexey <alexxey2707@gmail.com>
2024-06-15 17:14:14 +0200

    displaying cart items in cart modal

--- vishitiy_shop/cart/cart.py
+++ vishitiy_shop/cart/cart.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-
+from products.models import Product
+from decimal import Decimal
 
 class Cart:
     def __init__(self, request):
@@ -9,7 +10,29 @@ class Cart:
             cart = self.session[settings.CART_SESSION_KEY] = {}
         self.cart = cart
         
-    def __contains__(self, product_id):
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+            
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+        
+    def contains_deep(self, product_id, **params):
+        """
+            Check if product in cart with all identical params 
+            params: size=L, color=red, etc
+        """
+        if params:
+            for key, value in params.items():
+                if self.cart[product_id][key] != value:
+                    return False
+        return True
+        
+    def __contains__(self, product_id, **params):
         return str(product_id) in self.cart
         
     def add(self, **data) -> dict | None:
@@ -23,16 +46,18 @@ class Cart:
         self.session.modified = True
     
     def remove(self, product_id):
-
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-        else:
-            raise ValueError('Product not in cart')
-        
+        del self.cart[product_id]
+        self.save()
+        return None
+    
     def update(self, **data): 
         product_id = data.pop('product_id')
         if product_id in self.cart:
             self.cart[product_id].update(**data)
             self.save()
             return self.cart[product_id]
+        
+        
+    def clear(self):
+        del self.session[settings.CART_SESSION_KEY]
+        self.save()
