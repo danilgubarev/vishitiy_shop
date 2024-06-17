4699be9aae6bfcfdb0b7a0a569ce15b1b81359a1
danilgubarev2008 <gubarevdanil83@gmail.com>
2024-06-17 11:18:54 +0300

    add commentaries in the app [products]

--- vishitiy_shop/products/index.py
+++ vishitiy_shop/products/index.py
@@ -1,13 +1,21 @@
-from algoliasearch_django import AlgoliaIndex
-from algoliasearch_django.decorators import register
+from algoliasearch_django import AlgoliaIndex  
+# Импортируем класс AlgoliaIndex из пакета algoliasearch_django
+from algoliasearch_django.decorators import register  
+# Импортируем декоратор register из пакета algoliasearch_django
 
-from .models import Product
+from .models import Product  
+# Импортируем модель Product из текущего приложения
 
-@register(Product)
-class ProductIndex(AlgoliaIndex):
-    fields = ('title', 'type', 'url', 'image_url', 'price')
-    index_name = 'products'
-    settings = {
-        'searchableAttributes': ['title'],
-        'attributesForFaceting': ['color', 'size', 'type']
+@register(Product)  
+# Регистрируем модель Product для индексации в Algolia
+class ProductIndex(AlgoliaIndex):  
+    # Определяем класс ProductIndex, который наследует от AlgoliaIndex
+    fields = ('title', 'type', 'url', 'image_url', 'price')  
+    # Поля модели Product, которые будут индексироваться в Algolia
+    index_name = 'products'  
+    # Имя индекса в Algolia
+    settings = {  # Настройки индекса
+        'searchableAttributes': ['title'],  # Поля, которые будут использоваться для поиска
+        
+        'attributesForFaceting': ['color', 'size', 'type']  # Поля, которые будут использоваться для фильтрации
     }
